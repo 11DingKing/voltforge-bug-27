687d2dc627df63236dfc713aafdb760380ad00b5
为人民服务 <rking.ding@gmail.com>
2026-08-20 09:49:08 +0800

    fix: correct fast-charge behavior

--- internal/certification/certificationindex_adapter.go
+++ internal/certification/certificationindex_adapter.go
@@ -2,7 +2,7 @@ package certification
 
 func (i *CertificationIndexIndex) Load(bucket string) []string {
 	if i == nil || i.buckets == nil {
-		return nil
+		return []string{}
 	}
 	values := i.buckets[bucket]
 	out := make([]string, len(values))
--- internal/certification/certificationindex_service.go
+++ internal/certification/certificationindex_service.go
@@ -2,7 +2,12 @@ package certification
 
 type CertificationIndexIndex struct{ buckets map[string][]string }
 
-func NewCertificationIndexIndex() *CertificationIndexIndex { return &CertificationIndexIndex{} }
+func NewCertificationIndexIndex() *CertificationIndexIndex {
+	return &CertificationIndexIndex{buckets: make(map[string][]string)}
+}
 func (i *CertificationIndexIndex) Restore(bucket, value string) {
+	if i.buckets == nil {
+		i.buckets = make(map[string][]string)
+	}
 	i.buckets[bucket] = append(i.buckets[bucket], value)
 }
